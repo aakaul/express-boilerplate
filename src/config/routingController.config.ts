8445a7e74ff029b9c7439c756d242e005cc2c14c
abhey <ms.akaul@gmail.com>
2023-02-25 22:55:26 +0530

    deleted

--- src/config/routingController.config.ts
+++
@@ -1,8 +0,0 @@
-import path from "path"
-
-export default {
-	controllers: [path.join(__dirname,"/routes/api/**/*.controller.js")],
-	classTransformer: true,
-	defaultErrorHandler:true,
-	validation: false,
-}
