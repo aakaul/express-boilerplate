189ad5b452e3980ee40a19ef18cd979aa0514cac
abhey <ms.akaul@gmail.com>
2023-02-25 22:50:29 +0530

    constant update

--- src/utils/Contants.ts
+++ src/utils/Contants.ts
@@ -1,6 +1,6 @@
 export class Constants {
 	static discordSyncEntryChannel = "sync-entry-err";
-	static millisInOneDay = 8.65e7;
+	static millisInOneDay = 8.64e7;
 	static currMillis = () => Date.now();
 	static dateInstance = () => new Date();
 	static getLastStampToday=(stamp?:number)=> new Date(stamp).setHours(0,0,0,0)+8.65e7-1;
